d80f54107df00d75556ba10269fa21884d8f79af
Xander Whitby <xwhitby20@gmail.com>
2025-10-03 10:40:09 -0600

    pair works?

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,9 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  'windwp/nvim-autopairs',
-  config = function()
-    require('nvim-autopairs').setup {}
-  end,
-}
+return {}
