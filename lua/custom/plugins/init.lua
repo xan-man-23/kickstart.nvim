ef218aa8eb750bbaee44cc87456a52389ef97d07
Xander Whitby <xwhitby20@gmail.com>
2025-10-02 18:50:17 -0600

    added autopairs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'windwp/nvim-autopairs',
+  config = function()
+    require('nvim-autopairs').setup {}
+  end,
+}
